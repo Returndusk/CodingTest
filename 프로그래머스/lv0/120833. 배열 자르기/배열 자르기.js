a3212036d4343934ba6a513350ee2f73aa54407c
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-03-24 16:39:04 +0900

    [level 0] Title: 배열 자르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,8 +1,8 @@
 function solution(numbers, num1, num2) {
     var answer = [];
     
-    for (let i = num1; num1 <= num2; num1++)
-        answer.push(numbers[num1])
+    for (let i = num1; i <= num2; i++)
+        answer.push(numbers[i])
     
     return answer;
 }
