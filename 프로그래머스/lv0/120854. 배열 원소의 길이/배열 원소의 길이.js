4ea11e75de79992004c639ed7de551c62ef262fd
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-03-24 16:33:54 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,9 +1,3 @@
-function solution(strlist) {
-    var answer = [];
-    
-    for (let i = 0; i < strlist.length; i++) {
-        answer.push(strlist[i].length)
-    }
-    
-    return answer;
+function solution(strlist) {   
+    return strlist.map((a) => a.length)
 }
