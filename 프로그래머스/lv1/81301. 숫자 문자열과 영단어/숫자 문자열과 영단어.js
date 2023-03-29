86147f20969eb2fd81973ee7502ec739e75f5738
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-03-29 10:43:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,21 +1,11 @@
 function solution(s) {
-  var answer = s;
-  const num = [
-    ["zero", "0"],
-    ["one", "1"],
-    ["two", "2"],
-    ["three", "3"],
-    ["four", "4"],
-    ["five", "5"],
-    ["six", "6"],
-    ["seven", "7"],
-    ["eight", "8"],
-    ["nine", "9"],
-  ];
-  for (const a of num) {
-    answer = answer.replaceAll(a[0], a[1]);
-  }
-  return parseInt(answer);
-}
+    const alphabet = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    let answer = s;
 
-console.log(solution("2three45sixseven"));
+    for (let i = 0; i < alphabet.length; i++) {
+        let array = answer.split(alphabet[i]);
+        answer = array.join(i);
+    }
+
+    return Number(answer);
+}
