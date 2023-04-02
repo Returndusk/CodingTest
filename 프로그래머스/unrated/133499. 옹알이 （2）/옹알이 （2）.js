2f240be6398de025df5c549ed5ed348dccb7a639
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-04-03 01:39:04 +0900

    [unrated] Title: 옹알이 (2), Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -7,14 +7,17 @@ function solution(babbling) {
             for(let j = 0; j < speak.length; j++) {
                 let s = speak[j];
                 
+                // 이 애기는 같은 발음을 연속으로 할 수 없기에, 문자열 b에 s값이 두 번 연속으로 있을 경우 break
+                // 위의 경우가 아닌 경우 문자열 b의 s값을 공백으로 바꾸고 join으로 합침
                 if(b.includes(s.repeat(2))) {
                     break;
                 } else {    
-                    b = b.split(s).join('z');
+                    b = b.split(s).join(' ');
                 }
             }
-    
-            if(b.split('z').join('').length == 0) {
+            
+            // b 값에 공백 부분을 제거한 문자열의 길이가 0일 경우 result 값을 반환
+            if(b.split(' ').join('').length == 0) {
                 result++;
             }
         }
