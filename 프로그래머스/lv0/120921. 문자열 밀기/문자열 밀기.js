b0c963dd08cf551e10a8bbcab9c23642fdd0634d
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-07-04 11:58:36 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,14 @@
+function solution(A, B) {
+  if (A === B) {
+    return 0;
+  }
+
+  for (let i = 0; i <= A.length; i++) {
+    A = A[A.length - 1] + A.substring(0, A.length - 1);
+
+    if (A === B) {
+      return i + 1;
+    }
+  }
+  return -1;
+}
