d085a84b1162f9ff1797816d832a9d0e3b9a0c94
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-07-13 11:37:55 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,12 +1,13 @@
 function solution(s) {
   let array = s.split(' ');
   let answer = '';
-  
+
   for (let i = 0; i < array.length; i++) {
-    let str = array[i]
+    let str = array[i];
     for (let j = 0; j < str.length; j++) {
-      if (j % 2 === 0) answer += str[j].toUpperCase();
-      if (j % 2 === 1) answer += str[j].toLowerCase();
+      j % 2 === 0
+        ? (answer += str[j].toUpperCase())
+        : (answer += str[j].toLowerCase());
     }
     if (i < array.length - 1) answer += ' ';
   }
