ff045b777f64baef5be899284c25c728dd53230f
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-07-24 11:54:28 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let answer = '';
+  let start = true;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      answer += s[i];
+      start = true;
+    } else {
+      if (start) {
+        answer += s[i].toUpperCase();
+        start = false;
+      } else {
+        answer += s[i].toLowerCase();
+      }
+    }
+  }
+
+  return answer;
+}
