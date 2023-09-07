7a81dd23a719522d84dfb6697ab1dbb90419907b
김득열 <94781765+Returndusk@users.noreply.github.com>
2023-09-07 12:00:01 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.86 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -0,0 +1,51 @@
+function solution(m, n, board) {
+  board = board.map((v) => v.split(''));
+
+  while (true) {
+    let removeBlock = [];
+    // 2x2  찾기
+    for (let i = 1; i < m; i++) {
+      for (let j = 1; j < n; j++) {
+        if (
+          board[i][j] &&
+          board[i][j] === board[i - 1][j] &&
+          board[i][j] === board[i - 1][j - 1] &&
+          board[i][j] === board[i][j - 1]
+        ) {
+          removeBlock.push([i, j]);
+        }
+      }
+    }
+
+    if (!removeBlock.length) break;
+
+    // 블럭 없애기
+    removeBlock.forEach((v) => {
+      board[v[0]][v[1]] = 0;
+      board[v[0] - 1][v[1]] = 0;
+      board[v[0]][v[1] - 1] = 0;
+      board[v[0] - 1][v[1] - 1] = 0;
+    });
+
+    // 땅기기
+    for (let j = 0; j < n; j++) {
+      let pointer = m - 1;
+      for (let i = m - 1; i >= 0; i--) {
+        if (board[i][j] !== 0) {
+          board[pointer][j] = board[i][j];
+          if (pointer !== i) board[i][j] = 0;
+          pointer--;
+        }
+      }
+    }
+  }
+
+  let count = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 0) count++;
+    }
+  }
+
+  return count;
+}
